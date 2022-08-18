85cb51e03e9866e48b593aa570651e66bd02bc8e
Andre H <andre.guimaraes@itriad.org.br>
2022-08-17 21:05:12 -0400

    :tada: projeto criado com Spring

---
+++ src/main/kotlin/com/api/gerenciaNet/GerenciaNetApplication.kt
@@ -0,0 +1,11 @@
+package com.api.gerenciaNet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciaNetApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciaNetApplication>(*args)
+}
